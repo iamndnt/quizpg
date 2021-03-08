2519cfcb4ac8620a660870f9e441bc3548d3840a
Phan Huy Cuong <63645445+azuredragon3000@users.noreply.github.com>
2021-03-08 16:35:53 +0700

    Update SpecTrongHoaSenGame.java

--- app/src/main/java/com/myapp/quizpg/SpecTrongHoaSenGame.java
+++ app/src/main/java/com/myapp/quizpg/SpecTrongHoaSenGame.java
@@ -45,94 +45,94 @@ public class SpecTrongHoaSenGame extends ObjectSpec{
                 Color.argb(100,255,255,255),
                 Color.argb(100,255,255,255),
                 (int)mScreenSize.x/50,
-                0,0,ge,null);
+                0,0,ge,null,topic);
 
 
         rects[1] = new myRect("POS1",0,0,x/4,y/4,
                 Color.argb(255,255,255,255),
                 Color.argb(255,255,255,255),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
         rects[2] = new myRect("POS2",x/4,0,x/2,y/4,
                 Color.argb(255,255,255,255),
                 Color.argb(255,255,255,255),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
         rects[3] = new myRect("POS3",x/2,0,3*x/4,y/4,
                 Color.argb(255,255,255,255),
                 Color.argb(255,255,255,255),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
         rects[4] = new myRect("POS4",0,y/4,x/4,y/2,
                 Color.argb(255,255,255,255),
                 Color.argb(255,255,255,255),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
         rects[5] = new myRect("POS5",x/4,y/4,x/2,y/2,
                 Color.argb(255,255,255,255),
                 Color.argb(255,255,255,255),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
         rects[6] = new myRect("POS6",x/2,y/4,3*x/4,y/2,
                 Color.argb(255,255,255,255),
                 Color.argb(255,255,255,255),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
 
         rects[7] = new myRect("POS7",0,y/2,x/4,3*y/4,
                 Color.argb(255,255,255,255),
                 Color.argb(255,255,255,255),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
         rects[8] = new myRect("POS8",x/4,y/2,x/2,3*y/4,
                 Color.argb(255,255,255,255),
                 Color.argb(255,255,255,255),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
         rects[9] = new myRect("POS9",x/2,y/2,3*x/4,3*y/4,
                 Color.argb(255,255,255,255),
                 Color.argb(255,255,255,255),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
 
         rects[10] = new myRect("POS10",0,3*y/4,x/4,y,
                 Color.argb(255,255,255,255),
                 Color.argb(255,255,255,255),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
         rects[11] = new myRect("POS11",x/4,3*y/4,x/2,y,
                 Color.argb(255,255,255,255),
                 Color.argb(255,255,255,255),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
         rects[12] = new myRect("POS12",x/2,3*y/4,3*x/4,y,
                 Color.argb(255,255,255,255),
                 Color.argb(255,255,255,255),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
         rects[13] = new myRect("CỬA HÀNG",3*x/4,3*y/4,x,7*y/8,
                 Color.rgb(255,255,255),
                 Color.rgb(2,195,6),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
         rects[14] = new myRect("EXIT",3*x/4,7*y/8,x,y,
                 Color.rgb(255,255,255),
                 Color.rgb(2,195,6),
                 (int)mScreenSize.x/50,
-                padingx,padingx,ge,null);
+                padingx,padingx,ge,null,topic);
 
         controls = new ArrayList<>();
         controls.add(KHUNG,rects[0]);
